3dcfa07229b8377385e937589b0d29004a15ac3a
CamilaNevesLinder <camilaneveslindner@.com>
2025-12-08 10:37:30 -0300

    refactor: formatando numbers

--- main.js
+++ main.js
@@ -15,3 +15,27 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 3000);
+
+function formatarTelefone(numero) {
+  numero = numero.replace(/\D/g, "");
+
+  if (numero.length === 11) {
+    return numero.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+  }
+
+  if (numero.length === 10) {
+    return numero.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $2-$3");
+  }
+
+  return numero;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const numbers = document.querySelectorAll(".number");
+
+  numbers.forEach((n) => {
+    const texto = n.textContent.trim();
+    const formatado = formatarTelefone(texto);
+    n.textContent = formatado;
+  });
+});
