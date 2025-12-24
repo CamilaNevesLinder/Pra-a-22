cb815a9af9f102e7b3588d2ad58a1319e1dd49da
CamilaNevesLinder <camilaneveslindner@.com>
2025-12-24 01:02:09 -0300

    refactor: add animation in description

--- cursos.js
+++ cursos.js
@@ -4,12 +4,14 @@ icons.forEach((icon) => {
   icon.addEventListener("click", () => {
     const description = icon.nextElementSibling;
 
-    if (description.style.display === "block") {
-      description.style.display = "none";
-      icon.style.transform = "rotate(0deg)";
-    } else {
-      description.style.display = "block";
+    // Alterna a classe 'active' para animação
+    description.classList.toggle("active");
+
+    // Gira o ícone
+    if (description.classList.contains("active")) {
       icon.style.transform = "rotate(45deg)";
+    } else {
+      icon.style.transform = "rotate(0deg)";
     }
   });
 });
